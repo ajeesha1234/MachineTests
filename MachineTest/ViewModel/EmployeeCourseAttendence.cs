bdf751b52da2266557523ec47236841f0666796f
ajeesha1234 <k.a.ajeesha@gmail.com>
2024-07-20 13:10:18 +0530

    clearing...

---
+++ MachineTest/ViewModel/EmployeeCourseAttendence.cs
@@ -0,0 +1,13 @@
+﻿namespace MachineTest.ViewModel
+{
+    public class EmployeeCourseAttendence
+    {
+        
+            public string Employee { get; set; }
+            public string Course { get; set; }
+            public int Validity { get; set; }
+            public int AttendanceCount { get; set; }
+            public bool NeedsNotification { get; set; } // New property
+        
+    }
+}
